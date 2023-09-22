d4d638e7f5d4357a8dfe202f85c5c14eba6e8eae
Linko-3D <dbekhouc@gmail.com>
2023-09-22 18:33:33 +0200

    tweak

--- public/js/app.js
+++ public/js/app.js
@@ -4,13 +4,15 @@ document.addEventListener("DOMContentLoaded", function () {
     const arrowTop = document.getElementById("arrow-top");
 
     window.addEventListener("scroll", function () {
-      // Check the scroll position
-      if (window.scrollY > 200) {
-        // If the user has scrolled down, show the arrow
-        arrowTop.style.display = "block";
-      } else {
-        // If the user is at the top of the page, hide the arrow
-        arrowTop.style.display = "none";
-      }
+        // Check the scroll position
+        if (window.scrollY > 200) {
+            // If the user has scrolled down, show the arrow
+            arrowTop.style.transform = "translateX(0)"; 
+            arrowTop.style.opacity = "1"; // set the opacity to 1 (fully visible)
+        } else {
+            // If the user is at the top of the page, hide the arrow
+            arrowTop.style.transform = "translateX(150%)";
+            arrowTop.style.opacity = "0"; // set the opacity to 0 (fully transparent)
+        }
     });
-  });
+});
